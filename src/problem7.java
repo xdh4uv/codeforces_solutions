5d1780a2c39d082a96f48550f9f77386646f8e4b
xdh4uv <dhruvnainn@gmail.com>
2025-07-09 19:18:34 +0530

    hellp

--- src/problem7.java
+++ src/problem7.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+// lexographically find karo
+//
 public class problem7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] firstWord = br.readLine().split("");
+        String[] secondWord = br.readLine().split("");
+
+
+        
+
+
+
+    }
 }
